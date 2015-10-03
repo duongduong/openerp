91eee90bc8f56ab45d0e98d94a6af115519d975c
duongduong <huongduong328@gmail.com>
2015-10-03 12:57:46 +0700

    create new format report saleorder

---
+++ __openerp__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Project_test',
+    'version': '1.0',
+    'depends': ['sale'],
+    'author': 'DH',
+    'data': ['sale_order_report.xml'
+             'views/saleorder_report.xml'],
+    'auto_install':True,
+    'installable': True,
+}
